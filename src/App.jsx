363edf6a2830bab9b1133163080d692ef2696ed2
Moemen <98442887+M02men0@users.noreply.github.com>
2025-12-26 01:37:54 -0500

    Create App component with main structure

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import Intro from './pages/Intro.jsx';
+import About from './pages/About.jsx';
+import Projects from './pages/Projects.jsx';
+import Experience from './pages/Experience.jsx';
+import Skills from './pages/Skills.jsx';
+import Contact from './pages/Contact.jsx';
+
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Intro />
+      <About />
+      <Projects />
+      <Experience />
+      <Skills />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
